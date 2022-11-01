cbb8cb95d82fb00b947f91797625aac6c57a0561
vmorganp <100230514+MorganPattersonSignet@users.noreply.github.com>
2022-11-01 02:45:30 -0700

    make the unused opt key into a mute key

--- keyboards/sofle/keymaps/vmorganp/keymap.c
+++ keyboards/sofle/keymaps/vmorganp/keymap.c
@@ -19,11 +19,11 @@ static uint32_t jiggle_timer      = 0;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_OSX] = LAYOUT(
-  KC_GRV,   KC_1,         KC_2,         KC_3,         KC_4,         KC_5,   /*&&&&&&     &&&&&&*/      KC_6,       KC_7,         KC_8,         KC_9,         KC_0,            KC_BSPC,
-  KC_TAB,   KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   /*&&&&&&     &&&&&&*/      KC_Y,       KC_U,         KC_I,         KC_O,         KC_P,            LCTL(LSFT(LGUI(KC_4))),
-  KC_ESC,   LCTL_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LOPT_T(KC_F), KC_G,   /*&&&&&&     &&&&&&*/      KC_H,       LOPT_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LCTL_T(KC_SCLN), KC_ENT,
-  KC_LSFT,  KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   _______,     KC_MPLY,      KC_N,       KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
-                          KC_LALT,      KC_LCTL,      KC_LGUI,      KC_SPC, MO(_NAV),    MO(_SYMBOLS), KC_MS_BTN1, KC_MS_BTN3,   KC_MS_BTN2,   KC_CAPS 
+  KC_GRV,   KC_1,         KC_2,             KC_3,         KC_4,         KC_5,   /*&&&&&&     &&&&&&*/      KC_6,       KC_7,         KC_8,         KC_9,         KC_0,            KC_BSPC,
+  KC_TAB,   KC_Q,         KC_W,             KC_E,         KC_R,         KC_T,   /*&&&&&&     &&&&&&*/      KC_Y,       KC_U,         KC_I,         KC_O,         KC_P,            LCTL(LSFT(LGUI(KC_4))),
+  KC_ESC,   LCTL_T(KC_A), LGUI_T(KC_S),     LSFT_T(KC_D), LOPT_T(KC_F), KC_G,   /*&&&&&&     &&&&&&*/      KC_H,       LOPT_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LCTL_T(KC_SCLN), KC_ENT,
+  KC_LSFT,  KC_Z,         KC_X,             KC_C,         KC_V,         KC_B,   _______,     KC_MPLY,      KC_N,       KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
+                          LGUI(LSFT(KC_M)), KC_LCTL,      KC_LGUI,      KC_SPC, MO(_NAV),    MO(_SYMBOLS), KC_MS_BTN1, KC_MS_BTN3,   KC_MS_BTN2,   KC_CAPS 
 ),
 
 [_WINDOWS] = LAYOUT(
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______,  _______, _______, _______, /*&&&&&&   &&&&&&*/ _______, _______, _______, _______, _______, LSFT(LGUI(KC_S)),
   _______, _______, _______,  _______, _______, _______, /*&&&&&&   &&&&&&*/ _______, _______, _______, _______, _______, _______,
   _______, _______, _______,  _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______,
-                    KC_LALT,  KC_LGUI, KC_LCTL, KC_SPC,  _______,   _______, _______, _______, _______, _______ 
+                    A(KC_P0), KC_LGUI, KC_LCTL, KC_SPC,  _______,   _______, _______, _______, _______, _______ 
 ),
 
 [_GAMING] = LAYOUT(
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______,  _______, _______, _______, /*&&&&&&   &&&&&&*/ _______, _______, _______, _______, _______, _______,
   _______, KC_A,    KC_S,     KC_D,    KC_F,    _______, /*&&&&&&   &&&&&&*/ _______, _______, _______, _______, _______, _______,
   _______, _______, _______,  _______, _______, _______, KC_MPLY,   _______, _______, _______, _______, _______, _______, _______,
-                    KC_LCTL,  KC_LCTL, KC_SPC,  KC_SPC,  _______,   _______, _______, _______, _______, _______ 
+                    _______,  KC_LCTL, KC_SPC,  KC_SPC,  _______,   _______, _______, _______, _______, _______ 
 ),
 
 [_NAV] = LAYOUT(
