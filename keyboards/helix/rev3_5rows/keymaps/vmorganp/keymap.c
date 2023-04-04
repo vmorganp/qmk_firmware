727b8f4b08134d5f59441ed850ecfb9be6235e32
vmorganp <31448722+vmorganp@users.noreply.github.com>
2023-04-04 10:53:32 -0700

    better mute logic

--- keyboards/helix/rev3_5rows/keymaps/vmorganp/keymap.c
+++ keyboards/helix/rev3_5rows/keymaps/vmorganp/keymap.c
@@ -31,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_GRV,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,   /*&&&&&&     &&&&&&*/  KC_6,       KC_7,         KC_8,         KC_9,         KC_0,            KC_BSPC,
             KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,   /*&&&&&&     &&&&&&*/  KC_Y,       KC_U,         KC_I,         KC_O,         KC_P,            SCRNS,
             KC_ESC,  LCTL_T(KC_A), LGUI_T(KC_S), LSFT_T(KC_D), LOPT_T(KC_F), KC_G,   /*&&&&&&     &&&&&&*/  KC_H,       LOPT_T(KC_J), LSFT_T(KC_K), LGUI_T(KC_L), LCTL_T(KC_SCLN), KC_ENT,
-            KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_BSPC,     KC_MPLY,  KC_N,       KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
+            KC_LSFT, KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,   KC_BSPC,     MUTE,     KC_N,       KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RSFT,
             KC_LCTL, MUTE,         _______,      KC_LGUI,      KC_LCTL,      KC_SPC, MO(_NAV),    MO(_SYM), KC_MS_BTN1, KC_MS_BTN3,   KC_MS_BTN2,   KC_CAPS,      _______,         _______
-            ),
+    ),
 
     [_OSX] = LAYOUT(
             _______, _______, _______, _______, _______, _______, /*&&&&&&     &&&&&&*/ _______, _______, _______, _______, _______, _______,
@@ -51,14 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, KC_LALT, KC_SPC,  _______, _______,     _______, _______, _______, _______, _______, _______, _______
     ),
 
-
     [_NAV] = LAYOUT(
             _______, KC_F1,    KC_F2,    KC_F3,     KC_F4,   KC_F5, /*&&&&&&     &&&&&&*/  KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,
             _______, _______,  _______,  _______,   QK_LEAD, TERM,  /*&&&&&&     &&&&&&*/  _______, _______,  _______, _______, _______, KC_F12,
             _______, _______,  CHAT,     DISC,      KC_BSPC, GHPRS, /*&&&&&&     &&&&&&*/  KC_LEFT, KC_DOWN,  KC_UP,   KC_RGHT, _______, _______,
             _______, TO(_OSX), TO(_WIN), TO(_GAME), CODE,    BROWS, KC_DEL,      _______,  _______, MUSIC,    _______, _______, _______, _______,
             _______, _______,  _______,  _______,   _______, OVRVW, _______,     MO(_ADJ), _______, _______,  _______, _______, _______, _______
-            ),
+    ),
 
     [_SYM] = LAYOUT(
             _______, _______,  _______, _______,  _______,  _______, /*&&&&&&     &&&&&&*/ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
@@ -66,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______,  _______, _______,  KC_DEL,   _______, /*&&&&&&     &&&&&&*/ KC_DQT,  KC_QUOT, KC_LBRC, KC_RBRC, KC_BSLS, _______,
             _______, _______,  _______, _______,  _______,  _______, _______,     _______, KC_UNDS, KC_MINS, KC_LPRN, KC_RPRN, _______, _______,
             _______, _______,  _______, _______,  _______,  _______, MO(_ADJ),    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______
-            ),
+    ),
 
     [_ADJ] = LAYOUT(
             _______, _______, _______, _______, _______, _______, /*&&&&&&     &&&&&&*/ _______, _______, _______, _______, _______, _______,
@@ -74,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, _______, _______, _______, /*&&&&&&     &&&&&&*/ KC_VOLD, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______
-            )
+    )
 };
 
 
@@ -169,9 +168,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
 
         case MUTE:
-                if (record->event.pressed){
+            if (record->event.pressed){
+                if (biton32(layer_state) == _GAME)  {
                     tap_code16(A(KC_P0));
                 }
+                else {
+                    tap_code16(C(S(KC_M)));
+                }
+            }
             return false;
 
         case SCRNS:
